9ce67af21006b00e25b081cfd626e1d6ffe82776
Xingyu Wang <brandon.xyw@gmail.com>
2019-09-12 15:31:30 -0400

    change color base on mouse click

--- RGB_Color.cpp
+++ RGB_Color.cpp
@@ -9,104 +9,84 @@
 
 #include "stdio.h"
 #include <stdlib.h>
+#include <math.h>
 
 
 double side_length = 2.0;
-double alpha = 1.0;
-double screenWidth = 500; 
-double recLength = screenWidth;	
+double screenWidth = 500;
+double color_value =  1.0;
+double recLength = screenWidth;
+double rec_offset = 8.5;
+double rec_length = 8.5 * sqrt(2); 	
 
 void drawCube() {
 	// OK
 	glBegin(GL_POLYGON);
-  	glColor4f( 1.0, 0.0, 0.0, alpha);     glVertex3f(  side_length, -1 * side_length, -1 * side_length );      
-  	glColor4f( 1.0, 1.0, 0.0, alpha);     glVertex3f(  side_length,  side_length, -1 * side_length );      
-  	glColor4f( 0.0, 1.0, 0.0, alpha);     glVertex3f( -1 * side_length,  side_length, -1 * side_length );     
-  	glColor4f( 0.0, 0.0, 0.0, alpha);     glVertex3f( -1 * side_length, -1 * side_length, -1 * side_length );      
+  	glColor3f( color_value, 0.0, 0.0 );     		glVertex3f(  side_length, -1 * side_length, -1 * side_length );      
+  	glColor3f( color_value, color_value, 0.0 );     glVertex3f(  side_length,  side_length, -1 * side_length );      
+  	glColor3f( 0.0, color_value, 0.0);     			glVertex3f( -1 * side_length,  side_length, -1 * side_length );     
+  	glColor3f( 0.0, 0.0, 0.0 );     				glVertex3f( -1 * side_length, -1 * side_length, -1 * side_length );      
 	glEnd();
 
   	// Left
   	glBegin(GL_POLYGON);
-  	glColor4f( 1.0, 0.0, 1.0, alpha);		glVertex3f(  side_length, -1 * side_length, side_length );	
-  	glColor4f( 1.0, 1.0, 1.0, alpha);		glVertex3f(  side_length,  side_length, side_length );		// white
-  	glColor4f( 0.0, 1.0, 1.0, alpha);		glVertex3f( -1 * side_length,  side_length, side_length );
-  	glColor4f( 0.0, 0.0, 1.0, alpha);		glVertex3f( -1 * side_length, -1 * side_length, side_length );	// blue
+  	glColor3f( color_value, 0.0, color_value);				glVertex3f(  side_length, -1 * side_length, side_length );	
+  	glColor3f( color_value, color_value, color_value);		glVertex3f(  side_length,  side_length, side_length );		// white
+  	glColor3f( 0.0, color_value, color_value);				glVertex3f( -1 * side_length,  side_length, side_length );
+  	glColor3f( 0.0, 0.0, color_value);						glVertex3f( -1 * side_length, -1 * side_length, side_length );	// blue
   	glEnd();
  
   	// right
   	glBegin(GL_POLYGON);
-  	glColor4f( 1.0, 0.0, 0.0, alpha);		glVertex3f( side_length, -1 * side_length, -1 * side_length );
-  	glColor4f( 1.0, 1.0, 0.0, alpha);		glVertex3f( side_length,  side_length, -1 * side_length );
-  	glColor4f( 1.0, 1.0, 1.0, alpha);		glVertex3f( side_length,  side_length,  side_length );
-  	glColor4f( 1.0, 0.0, 1.0, alpha);		glVertex3f( side_length, -1 * side_length,  side_length );
+  	glColor3f( color_value, 0.0, 0.0);						glVertex3f( side_length, -1 * side_length, -1 * side_length );
+  	glColor3f( color_value, color_value, 0.0);				glVertex3f( side_length,  side_length, -1 * side_length );
+  	glColor3f( color_value, color_value, color_value);		glVertex3f( side_length,  side_length,  side_length );
+  	glColor3f( color_value, 0.0, color_value);				glVertex3f( side_length, -1 * side_length,  side_length );
   	glEnd();
  
   	// Green side  LEFT	OK
   	glBegin(GL_POLYGON);
-  	glColor4f( 0.0, 0.0, 1.0, alpha);		glVertex3f( -1 * side_length, -1 * side_length,  side_length );
-  	glColor4f( 0.0, 1.0, 1.0, alpha);		glVertex3f( -1 * side_length,  side_length,  side_length );
-  	glColor4f( 0.0, 1.0, 0.0, alpha);		glVertex3f( -1 * side_length,  side_length, -1 * side_length );
-  	glColor4f( 0.0, 0.0, 0.0, alpha);		glVertex3f( -1 * side_length, -1 * side_length, -1 * side_length );
+  	glColor3f( 0.0, 0.0, color_value);				glVertex3f( -1 * side_length, -1 * side_length,  side_length );
+  	glColor3f( 0.0, color_value, color_value);		glVertex3f( -1 * side_length,  side_length,  side_length );
+  	glColor3f( 0.0, color_value, 0.0);				glVertex3f( -1 * side_length,  side_length, -1 * side_length );
+  	glColor3f( 0.0, 0.0, 0.0);						glVertex3f( -1 * side_length, -1 * side_length, -1 * side_length );
   	glEnd();
  
   	// top
   	glBegin(GL_POLYGON);
-  	glColor4f( 1.0, 1.0, 1.0, alpha);		glVertex3f(  side_length,  side_length,  side_length );				// white
-  	glColor4f( 1.0, 1.0, 0.0, alpha);		glVertex3f(  side_length,  side_length, -1 * side_length );			// yellow
-  	glColor4f( 0.0, 1.0, 0.0, alpha);		glVertex3f( -1 * side_length,  side_length, -1 * side_length );		// green
-  	glColor4f( 0.0, 1.0, 1.0, alpha);		glVertex3f( -1 * side_length,  side_length,  side_length );			// green + blue
+  	glColor3f( color_value, color_value, color_value);		glVertex3f(  side_length,  side_length,  side_length );				// white
+  	glColor3f( color_value, color_value, 0.0);				glVertex3f(  side_length,  side_length, -1 * side_length );			// yellow
+  	glColor3f( 0.0, color_value, 0.0);						glVertex3f( -1 * side_length,  side_length, -1 * side_length );		// green
+  	glColor3f( 0.0, color_value, color_value);				glVertex3f( -1 * side_length,  side_length,  side_length );			// green + blue
   	glEnd();
  
   	// Red side  BOTTOM
   	glBegin(GL_POLYGON);
-  	glColor4f( 1.0, 0.0, 0.0, alpha);		glVertex3f(  side_length, -1 * side_length, -1 * side_length );
-  	glColor4f( 1.0, 0.0, 1.0, alpha);		glVertex3f(  side_length, -1 * side_length,  side_length );
-  	glColor4f( 0.0, 0.0, 1.0, alpha);		glVertex3f( -1 * side_length, -1 * side_length,  side_length );
-  	glColor4f( 0.0, 0.0, 0.0, alpha);		glVertex3f( -1 * side_length, -1 * side_length, -1 * side_length );
+  	glColor3f( color_value, 0.0, 0.0);				glVertex3f(  side_length, -1 * side_length, -1 * side_length );
+  	glColor3f( color_value, 0.0, color_value);		glVertex3f(  side_length, -1 * side_length,  side_length );
+  	glColor3f( 0.0, 0.0, color_value);				glVertex3f( -1 * side_length, -1 * side_length,  side_length );
+  	glColor3f( 0.0, 0.0, 0.0);						glVertex3f( -1 * side_length, -1 * side_length, -1 * side_length );
   	glEnd();
 }
 
 
-float recLengthConvert(double length) {
-	if (length > screenWidth / 2)
-	{
-		return (length - (screenWidth / 2)) / (screenWidth / 2);
-	} else if (length < 250) {
-		return ((screenWidth / 2) - (length )) / (screenWidth / 2) * -1.0f;
-	} else if (length == 250) {
-		return 0;
-	} else {
-		return -2.0f;	// ERROR
-	}
-}
+
 
 // draw the white bar at the bottom of the scree
 void drawRec() 
 {
 	glBegin(GL_POLYGON);
-  	double offset = 6.5;
-	glColor4f( 1.0, 1.0, 1.0, alpha);		
-	glVertex3f( 0 , -2.3, side_length + offset);		// top left
-  	glVertex3f( 0 , -3.5, side_length + offset );		// bottom left
-	glVertex3f( side_length + offset, -3.5, 0 );		// bottom right 
-  	glVertex3f( side_length + offset, -2.3, 0 );		// top right
+	glColor3f( 1, 1, 1);		
+	glVertex3f( 0 , -2.3, rec_offset);		// top left
+  	glVertex3f( 0 , -3.5, rec_offset );		// bottom left
+	glVertex3f( rec_offset, -3.5, 0 );		// bottom right 
+  	glVertex3f( rec_offset, -2.3, 0 );		// top right
   	glEnd();
 }
 
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-	double clipping  = 6;
-    glOrtho(-1 * clipping, clipping, -1 * clipping, clipping, -1 * clipping, clipping);
-	glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-	gluLookAt(	1.0, 1.0, 1.0, 
-				0.0, 0.0, 0.0,
-				0.0, 1.0, 0.0 );
 
 	drawCube();
 	drawRec();
@@ -127,11 +107,33 @@ void processMouse(int button,int state,int x,int y)
 	{
 		if (y >= 475){
 			printf("x: %d, y: %d\n", x, y);
-			recLength = x;
-			printf("conversion: %f\n", recLengthConvert(x));
+			//recLength = x;
+			color_value = 1.0 * x / screenWidth;		// change color base on length of slider
+			//printf("conversion: %f\n", recLengthConvert(x));
 			display();
 		}
 	} 
+}
+
+
+
+void init() 
+{
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+
+	glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+	double clipping  = 6;
+    glOrtho(-1 * clipping, clipping, -1 * clipping, clipping, -1 * clipping, clipping);
+	glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+	gluLookAt(	side_length / 2, side_length / 2, side_length / 2, 
+				0.0, 0.0, 0.0,
+				0.0, side_length / 2, 0.0 );
 }
 
 int main(int argc, char *argv[])
@@ -142,18 +144,13 @@ int main(int argc, char *argv[])
 	glutInitWindowPosition(400, 400);
 	glutCreateWindow("RGB_Cube");
 
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LESS);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
-
+	init();
 
 	// register callback func
 	glutDisplayFunc(display);
 	glutKeyboardFunc(processNormalKeys);
 	glutMouseFunc(processMouse);
-
-
+	
 	glutMainLoop();
 	return 0;
 }
